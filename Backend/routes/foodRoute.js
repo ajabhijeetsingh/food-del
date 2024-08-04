b212fb3a815b6a7abcdcbdd591983d39c9562977
ajabhijeetsingh <abhijeetsingh250.as@gmail.com>
2024-08-04 08:35:10 +0530

    main

--- Backend/routes/foodRoute.js
+++ Backend/routes/foodRoute.js
@@ -7,19 +7,19 @@ const foodRouter = express.Router();
 
 // Image Storage Engine
 
-const storage = multer.diskStorage({
-    // destination: "uploads",
-    destination: function (req, file, cb) {
-        cb(null, './uploads'); // Assuming 'uploads' directory exists and is writable
-    },
-    filename:(req,file,cb) =>{
-        return cb(null,`${Date.now()}${file.originalname}`)
-    }
-})
+// const storage = multer.diskStorage({
+//     // destination: "uploads",
+//     destination: function (req, file, cb) {
+//         cb(null, './uploads'); // Assuming 'uploads' directory exists and is writable
+//     },
+//     filename:(req,file,cb) =>{
+//         return cb(null,`${Date.now()}${file.originalname}`)
+//     }
+// })
 
-const upload = multer({storage: storage})
+// const upload = multer({storage: storage})
 
-foodRouter.post("/add",upload.single("image"),addFood);
+// foodRouter.post("/add",upload.single("image"),addFood);
 foodRouter.get("/list",listFood)
 foodRouter.post("/remove",removeFood)
 
